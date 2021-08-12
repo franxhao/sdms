0d455bb179edbfa272462e76fe0bbb42e4573204
gepeng <>
2021-08-12 15:09:49 +0800

    超级管理员添加一个普通用户和管理员用户，并且将注册的密码经过Md5加密

--- src/main/java/com/hqyj/twelve/dao/AdminDao.java
+++ src/main/java/com/hqyj/twelve/dao/AdminDao.java
@@ -1,9 +1,18 @@
 package com.hqyj.twelve.dao;
 
+import com.hqyj.twelve.pojo.Administrator;
+import com.hqyj.twelve.pojo.Employee;
+
 /**
  * @description:
  * @author: GP
  * @date: 2021/8/12-12:23
  */
 public interface AdminDao {
+    //添加一个管理员用户
+    void insertAdmin(Administrator admin);
+    //添加一个普通员工用户
+    void insertEmployee(Employee employee);
+
+
 }
---
+++ src/main/java/com/hqyj/twelve/realm/MyShiroRealm.java
@@ -0,0 +1,25 @@
+package com.hqyj.twelve.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * @description:
+ * @author: GP
+ * @date: 2021/8/12-14:19
+ */
+public class MyShiroRealm extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
