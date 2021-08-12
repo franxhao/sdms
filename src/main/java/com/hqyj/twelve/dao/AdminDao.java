4bb0b56209239d23685bfae892d5c17fc8ebba17
gepeng <>
2021-08-12 18:49:14 +0800

    管理员的dao层   crud操作 以及测试

--- src/main/java/com/hqyj/twelve/dao/AdminDao.java
+++ src/main/java/com/hqyj/twelve/dao/AdminDao.java
@@ -3,6 +3,8 @@ package com.hqyj.twelve.dao;
 import com.hqyj.twelve.pojo.Administrator;
 import com.hqyj.twelve.pojo.Employee;
 
+import java.util.List;
+
 /**
  * @description:
  * @author: GP
@@ -10,9 +12,34 @@ import com.hqyj.twelve.pojo.Employee;
  */
 public interface AdminDao {
     //添加一个管理员用户
-    void insertAdmin(Administrator admin);
+    int  insertAdmin(Administrator admin);
     //添加一个普通员工用户
-    void insertEmployee(Employee employee);
+    int  insertEmployee(Employee employee);
+
+    //查询所有管理员用户
+    List<Administrator> queryAllAdmin();
+
+    //查询所有普通用户
+    List<Employee>   queryAllEmp();
+
+    //根据用户名查询管理员
+    Administrator queryAdminByUsername(String username);
+
+    //根据用户名查询普通员工
+    Employee queryEmpByUsername(String username);
+
+
+    //根据id修改普通用户密码
+    int updateEmpPasswordById(Employee emp);
+
+    //根据id修改管理员用户密码
+    int  updateAdminPasswordById(Administrator admin);
+
+    //根据id修改普通用户密码
+    int deleteEmpById(Integer id);
+
+    //根据id修改管理员用户密码
+    int deleteAdminById(Integer id);
 
 
 }
--- src/test/java/com/hqyj/twelve/dao/AdminDaoTest.java
+++ src/test/java/com/hqyj/twelve/dao/AdminDaoTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -27,7 +29,7 @@ public class AdminDaoTest {
     @Test
     public void insertAdmin() {
         Administrator admin = new Administrator();
-        admin.setAdUsername("豪桑");
+        admin.setAdUsername("谢桑");
         admin.setAdPassword("123456");
         admin.setAdName("梁豪");
         admin.setAdSex("男");
@@ -48,7 +50,7 @@ public class AdminDaoTest {
     @Test
     public void insertEmployee() {
         Employee emp = new Employee();
-        emp.setEmpUsername("阳桑");
+        emp.setEmpUsername("瑶桑");
         emp.setEmpPassword("123456");
         emp.setEmpName("阳东霖");
         emp.setEmpSex("男");
@@ -64,5 +66,67 @@ public class AdminDaoTest {
         //将加密密码再次存入emp中。
         emp.setEmpPassword(hashPassword);
         adminDao.insertEmployee(emp);
+    }
+
+    @Test
+    public void queryAllAdmin(){
+        List<Administrator> administrators = adminDao.queryAllAdmin();
+        System.out.println(administrators);
+    }
+
+    @Test
+    public void queryAllEmp(){
+        List<Employee> employees = adminDao.queryAllEmp();
+        System.out.println(employees);
+    }
+
+    @Test
+    public void updateEmpPassword(){
+        Employee emp = new Employee();
+        emp.setEmpId(1);
+        //通过shiro框架加密密码
+        String hashPassword = MD5Util.Md5Hash("123456", "豪桑");
+        //将加密密码再次存入emp中。
+        emp.setEmpPassword(hashPassword);
+
+        adminDao.updateEmpPasswordById(emp);
+
+    }
+
+    @Test
+    public void updateAdminPassword(){
+        Administrator admin = new Administrator();
+        admin.setAdId(3);
+        //通过shiro框架加密密码
+        String hashPassword = MD5Util.Md5Hash("123456", "豪桑");
+        //将加密密码再次存入emp中。
+        admin.setAdPassword(hashPassword);
+        adminDao.updateAdminPasswordById(admin);
+    }
+
+
+
+    @Test
+    public void deleteEmpById(){
+        int num = adminDao.deleteEmpById(2);
+        System.out.println(num);
+    }
+
+    @Test
+    public void deleteAdminById(){
+        int i = adminDao.deleteAdminById(2);
+        System.out.println(i);
+    }
+
+    @Test
+    public void queryAdminByUsername(){
+        Administrator administrator = adminDao.queryAdminByUsername("梁%");
+        System.out.println(administrator);
+    }
+
+    @Test
+    public void queryEmpByUsername(){
+        Employee employee = adminDao.queryEmpByUsername("阳%");
+        System.out.println(employee);
     }
 }
