52d3b1fece06e56f553e708d8e56a58707659a3e
yangdonglin <2411582795@qq.com>
2021-08-16 19:24:42 +0800

    使用excel批量添加学生

--- src/main/java/com/hqyj/twelve/controller/StudentManagementController.java
+++ src/main/java/com/hqyj/twelve/controller/StudentManagementController.java
@@ -3,13 +3,19 @@ package com.hqyj.twelve.controller;
 import com.hqyj.twelve.pojo.PageData;
 import com.hqyj.twelve.pojo.Student;
 import com.hqyj.twelve.service.StudentManagementService;
+import com.hqyj.twelve.utils.POIUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -80,4 +86,34 @@ public class StudentManagementController {
         }
         return map;
     }
+
+    @RequestMapping("/upload")
+    public String upload(MultipartFile excelFile){
+        try {
+            //解析excel文件
+            List<String[]> strings = POIUtils.readExcel(excelFile);
+            //用于将解析出来的字符串转换为Date类型
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+            //对解析出的结果进行遍历
+            for(String[] arr : strings){
+                Student student = new Student();
+                student.setStuKey(arr[0]);
+                student.setStuName(arr[1]);
+                student.setStuSex(arr[2]);
+                student.setStuEnr(sdf.parse(arr[3]));
+                student.setStuBirth(sdf.parse(arr[4]));
+                student.setStuCollege(arr[5]);
+                student.setStuMajor(arr[6]);
+                student.setStuClass(arr[7]);
+                student.setStuAddress(arr[8]);
+                student.setStuTel(arr[9]);
+                studentManagementService.addStuforStudent(student);
+            }
+            return "redirect:/";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/";
+        }
+    }
+
 }
